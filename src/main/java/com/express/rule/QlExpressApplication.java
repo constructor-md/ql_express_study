bc5748a4a11675e9b0cab1e22492e0f8fb446bf6
constructor-md <826118236@qq.com>
2021-02-08 23:32:57 +0800

    init resp and add some config

---
+++ src/main/java/com/express/rule/QlExpressApplication.java
@@ -0,0 +1,13 @@
+package com.express.rule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QlExpressApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QlExpressApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/express/rule/demo.java
@@ -0,0 +1,33 @@
+package com.express.rule;
+
+import com.ql.util.express.DefaultContext;
+import com.ql.util.express.ExpressRunner;
+import com.ql.util.express.IExpressContext;
+
+public class demo {
+
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+
+    public static void express(){
+
+        ExpressRunner expressRunner = new ExpressRunner();
+
+
+        IExpressContext<String, Object> expressContext = new DefaultContext<>();
+
+
+
+
+
+    }
+
+
+
+
+}
